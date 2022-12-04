7fd6978a17f49f480a578626286a7cb027304980
Sandor Magyar <sandorzm@gmail.com>
2022-12-04 00:53:25 -0500

    Solve day 2 part 2

--- day2/Part1.hs
+++ day2/Part1.hs
@@ -1,10 +1,11 @@
-module Part1 where
+module Part1 (ordDiff) where
 
 import Data.Char
 
 ordDiff :: Char -> String -> Int
 ordDiff c s = ord (head s) - ord c
 
+-- (our - their + 1) `mod` 3 gives 0 for loss, 1 for tie, 2 for win
 roundScore :: Int -> Int -> Int
 roundScore their our = 1 + our + (our - their + 1) `mod` 3 * 3
 
---
+++ day2/Part2.hs
@@ -0,0 +1,15 @@
+module Part2 where
+
+import Part1 (ordDiff)
+
+roundScore :: Int -> Int -> Int
+roundScore their outcome = 1 + (their + outcome - 1) `mod` 3 + outcome * 3
+
+plannedScore :: [String] -> Int
+plannedScore (their:outcome:rest) = roundScore (ordDiff 'A' their) (ordDiff 'X' outcome)
+                                    + plannedScore rest
+plannedScore _ = 0
+
+main = do
+  guide <- readFile "input.txt"
+  print (plannedScore (words guide))

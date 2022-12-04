607fb7f7d22036c23bd5eddb9419774d53089584
Sandor Magyar <sandorzm@gmail.com>
2022-12-04 17:09:06 -0500

    Make day 1 paragraph splitter mimic 'lines'
    String splitting is a little more nuanced than I thought.

--- day1/Part1.hs
+++ day1/Part1.hs
@@ -2,8 +2,8 @@ module Part1 ( maxParagraphs ) where
 
 -- Returns list of paragraphs, each a list of lines
 paragraphLines :: [String] -> [[String]]
+paragraphLines [] = [] -- Treat empty file as zero elves (somewhat subjective)
 paragraphLines lines = case break null lines of
-                         ([], _)    -> []
                          (par, [])  -> [par]
                          (par, _:t) -> par : paragraphLines t
 
